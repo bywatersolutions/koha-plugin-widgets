8a8f8867be77f5c6ed59d31d1836ea6151852c6c
Kyle M Hall <kyle@bywatersolutions.com>
2019-11-01 07:19:33 -0400

    Add a caching mechanism

--- Koha/Plugin/Com/ByWaterSolutions/Widgets/WidgetsController.pm
+++ Koha/Plugin/Com/ByWaterSolutions/Widgets/WidgetsController.pm
@@ -24,6 +24,7 @@ use Template;
 use C4::Letters;
 use C4::Output;
 use C4::Reports::Guided;
+use Koha::Caches;
 use Koha::Reports;
 
 =head1 API
@@ -37,29 +38,46 @@ use Koha::Reports;
 sub render_widget {
     my $c = shift->openapi->valid_input or return;
 
-    my $module = $c->validation->param('module');
-    my $code = $c->validation->param('code');
+    my $module    = $c->validation->param('module');
+    my $code      = $c->validation->param('code');
     my $report_id = $c->validation->param('report_id');
 
-    my $letter = C4::Letters::getletter( $module, $code, undef, 'print' );
-    my $tt_markup = $letter->{content};
+    my $expiration_seconds = 60 * 15;    #TODO: Make a parameter with a default
 
-    # Should we use svc/report directly here?
-    my $report = Koha::Reports->find( $report_id );
-    my $sql = $report->savedsql;
-    my $offset = 0;
-    my $limit  = C4::Context->preference("SvcMaxReportRows") || 10;
-    my @sql_params; # Not supported yet
-    my ( $sth, $errors ) = execute_query( $sql, $offset, $limit, \@sql_params, $report_id );
-    my $rows = $sth->fetchall_arrayref({});
-
-    my $tt = Template->new();
-    my $vars = { rows => $rows };
     my $output;
-    
-    $tt->process( \$tt_markup, $vars, \$output );
 
-    return $c->render( status => 200, openapi => { html => $output } );
+    my $namespace    = "$module:$code:$report_id";
+    my $cache_key    = "widgets:widget:$namespace";
+    my $cache        = Koha::Caches->get_instance();
+    my $cache_active = $cache->is_cache_active;
+    if ($cache_active) {
+        $output = $cache->get_from_cache($cache_key);
+    }
+
+    unless ($output) {
+        my $letter = C4::Letters::getletter( $module, $code, undef, 'print' );
+        my $tt_markup = $letter->{content};
+
+        my $report = Koha::Reports->find($report_id);
+        my $sql    = $report->savedsql;
+        my $offset = 0;
+        my $limit  = C4::Context->preference("SvcMaxReportRows") || 10;
+        my @sql_params;    #TODO: Add support for sql params
+        my ( $sth, $errors ) =
+          execute_query( $sql, $offset, $limit, \@sql_params, $report_id );
+        my $rows = $sth->fetchall_arrayref( {} );
+
+        my $tt   = Template->new();
+        my $vars = { rows => $rows };
+
+        $tt->process( \$tt_markup, $vars, \$output );
+
+        if ($cache_active) {
+            $cache->set_in_cache( $cache_key, $rendered_widget,
+                { expiry => $report_rec->cache_expiry } );
+        }
+    }
+    return $c->render( status => 200, openapi => { widget => $output } );
 }
 
 1;
